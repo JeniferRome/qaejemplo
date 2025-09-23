6319f838034fcfe3a33b885862408cada86a42c7
Jenifer <jeniferromeroerazo@gmail.com>
2025-09-23 17:30:52 -0400

    actualizo archivo

---
+++ test_operaciones.py
@@ -0,0 +1,32 @@
+import pytest
+import calculadora
+
+
+def test_sumar():
+    assert calculadora.sumar(2,1) == 3
+
+
+
+
+def test_division_por_cero():
+     with pytest.raises(ValueError):
+         calculadora.dividir(10,0)
+
+
+@pytest.mark.parametrize("a,b,esperado",[
+    (2,5,7), #numeros positivos
+    (-4,-2,-6), #numeros negativos
+    (0,0,0) #numeros ceros
+])
+def test_sumar_varios(a,b,esperado):
+    assert calculadora.sumar(a,b) == esperado
+
+
+
+    def test_restar_com_fixture(numeros):
+        a,b = numeros
+        assert calculadora.restar(a,b) == 0
+
+    def test_sumar_com_fixture(numeros):
+        a,b = numeros
+        assert calculadora.sumar(a,b) == 10
